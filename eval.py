1f02d69964998a2897e1fba4422ec37a53ddb60c
danil1411 <32360659+danil1411@users.noreply.github.com>
2021-02-16 17:23:45 +0200

    Add files via upload

---
+++ eval.py
@@ -0,0 +1,75 @@
+import sys
+from PyQt5.QtGui import QFont, QTextDocument
+from PyQt5.QtWidgets import QGridLayout, QWidget, QLabel, QVBoxLayout, QApplication,QPushButton
+
+
+class MyExample(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle('Calculator')
+        self.form()
+    def form(self):
+
+        self.setGeometry(485,550,300,400)
+        self.label=QLabel('')
+        self.lbl=QLabel('')
+        font = QFont('Times', 10, QFont.Bold)
+        self.label.setFont(font)
+
+        layout = QGridLayout(self)
+        layout.addWidget(self.label)
+
+
+
+        self.btns=   ['','','C','Del',
+                      '1','2','3','/',
+                      '4','5','6','*',
+                      '7','8','9','-',
+                      '0','.','=','+']
+        y=0
+        pos= [(0,0),(0,1),(0,2),(0,3),
+              (1,0),(1,1),(1,2),(1,3),
+              (2,0),(2,1),(2,2),(2,3),
+              (3,0),(3,1),(3,2),(3,3),
+              (4,0),(4,1),(4,2),(4,3)]
+        for b in self.btns:
+            buttons=QPushButton(b)
+            if y==0:
+                layout.addWidget(self.label,0,0)
+            elif y==1:
+                layout.addWidget(self.lbl,0,1)
+            else:
+                layout.addWidget(buttons, pos[y][0], pos[y][1])
+                if buttons.text().isdigit() or buttons.text()== '+' \
+                   or buttons.text()== '-' or buttons.text()=='*' or buttons.text()=='/':
+                    buttons.clicked.connect(self.buttonclicked)
+                if buttons.text()== '=' :
+                    buttons.clicked.connect(self.eval)
+                if buttons.text()=='C':
+                    buttons.clicked.connect(self.C)
+                if buttons.text()=='Del':
+                    buttons.clicked.connect(self.Del)
+
+            y+=1
+    def buttonclicked(self):
+        sender =self.sender()
+        res=self.label.text()
+        self.label.setText(res+sender.text())
+    def eval(self):
+        s=eval(self.label.text())
+        self.lbl.setText('')
+        self.label.setText(str(s))
+    def C(self):
+        self.label.setText('')
+        self.lbl.setText('')
+    def Del(self):
+        self.lbl.setText('')
+        self.label.setText(self.label.text()[0:-1])
+
+
+if __name__=='__main__':
+    app = QApplication(sys.argv)
+    qb=MyExample()
+    qb.show()
+    sys.exit(app.exec())
